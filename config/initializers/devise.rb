27d8c28fc0255a44b34160f6e446ad679b01f34a
leeroy <leeroy.mcevoy@gmail.com>
2014-11-15 19:47:07 -0500

    FUCK

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -255,4 +255,6 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  ActionMailer::Base.default_url_options = { :host => "localhost:3000" }
 end
