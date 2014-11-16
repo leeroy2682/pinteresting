d44cfe393f5739c3a55bbf5eb52be298477693ac
leeroy <leeroy.mcevoy@gmail.com>
2014-11-15 19:34:28 -0500

    please work

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Pinteresting
 
     # required for heroku
     config.assets.initialize_on_precompile = false
+    config.action_mailer.default_url_options = { host: "gmail.com" }
   end
 end
